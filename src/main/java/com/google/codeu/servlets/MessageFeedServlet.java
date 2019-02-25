9e7bb828f560f54bbd400f770aa3a8cdcb30ac72
Jessica Meng <jessica.meng0402@gmail.com>
2019-02-24 21:54:35 -0800

    Step 4

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,11 +1,16 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import com.google.gson.Gson;
 
 /**
  * Handles fetching all messages for the public feed.
@@ -13,38 +18,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/feed")
 public class MessageFeedServlet extends HttpServlet{
 
+    private Datastore datastore;
+
+    @Override
+    public void init() {
+        datastore = new Datastore();
+    }
+
+    /**
+     * Responds with a JSON representation of Message data for all users.
+     */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
-        response.getOutputStream().println("this will be my message feed");
+        response.setContentType("application/json");
+
+        List<Message> messages = datastore.getAllMessages();
+        Gson gson = new Gson();
+        String json = gson.toJson(messages);
+
+        response.getOutputStream().println(json);
     }
 }
-
-/**public List<Message> getAllMessages(){
-        List<Message> messages = new ArrayList<>();
-
-        Query query = new Query("Message")
-                .addSort("timestamp", SortDirection.DESCENDING);
-        PreparedQuery results = datastore.prepare(query);
-
-        for (Entity entity : results.asIterable()) {
-            try {
-                String idString = entity.getKey().getName();
-                UUID id = UUID.fromString(idString);
-                String user = (String) entity.getProperty("user");
-                String text = (String) entity.getProperty("text");
-                long timestamp = (long) entity.getProperty("timestamp");
-
-                Message message = new Message(id, user, text, timestamp);
-                messages.add(message);
-            } catch (Exception e) {
-                System.err.println("Error reading message.");
-                System.err.println(entity.toString());
-                e.printStackTrace();
-            }
-        }
-
-        return messages;
-    }
- */
