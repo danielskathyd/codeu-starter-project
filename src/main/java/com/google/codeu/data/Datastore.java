28f39cac217717cf5e5dc780f449464bae6df83f
Jessica Meng <jessica.meng0402@gmail.com>
2019-03-07 23:45:12 -0800

    tring style checker again:

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -30,25 +30,20 @@ import java.util.UUID;
 
 /** Provides access to the data stored in Datastore. */
 public class Datastore {
+    private DatastoreService datastore;
 
-  private DatastoreService datastore;
-
-  public Datastore() {
+    public Datastore() {
     datastore = DatastoreServiceFactory.getDatastoreService();
   }
 
-  /**
-   * Stores the Message in Datastore.
-   */
   public void storeMessage(Message message) {
-    Entity messageEntity = new Entity("Message", message.getId().toString());
-    messageEntity.setProperty("user", message.getUser());
-    messageEntity.setProperty("text", message.getText());
-    messageEntity.setProperty("timestamp", message.getTimestamp());
-    messageEntity.setProperty("recipient", message.getRecipient());
-
-    datastore.put(messageEntity);
-  }
+        Entity messageEntity = new Entity("Message", message.getId().toString());
+        messageEntity.setProperty("user", message.getUser());
+        messageEntity.setProperty("text", message.getText());
+        messageEntity.setProperty("timestamp", message.getTimestamp());
+        messageEntity.setProperty("recipient", message.getRecipient());
+        datastore.put(messageEntity);
+    }
 
   /**
    * Returns the total number of messages for all users.
@@ -66,7 +61,7 @@ public class Datastore {
    * message. List is sorted by time descending.
    */
   public List<Message> getMessages(String recipient) {
-      List<Message> messages = new ArrayList<>();
+    List<Message> messages = new ArrayList<>();
     Query query =
               new Query("Message")
                       .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
@@ -93,32 +88,29 @@ public class Datastore {
 
       return messages;
   }
-
   public List<Message> getAllMessages(){
-	List<Message> messages = new ArrayList<>();
-
-	  Query query = new Query("Message")
-	    .addSort("timestamp", SortDirection.DESCENDING);
+      List<Message> messages = new ArrayList<>();
+      Query query = new Query("Message")
+              .addSort("timestamp", SortDirection.DESCENDING);
 	  PreparedQuery results = datastore.prepare(query);
 
 	  for (Entity entity : results.asIterable()) {
-	   try {
-	    String idString = entity.getKey().getName();
-	    UUID id = UUID.fromString(idString);
-	    String user = (String) entity.getProperty("user");
-	    String text = (String) entity.getProperty("text");
-	    long timestamp = (long) entity.getProperty("timestamp");
-	    
-	    String recipient = (String) entity.getProperty("recipient");
-
-	    Message message = new Message(id, user, text, timestamp, recipient);
-	    messages.add(message);
-	   } catch (Exception e) {
-	    System.err.println("Error reading message.");
-	    System.err.println(entity.toString());
-	    e.printStackTrace();
-	   }
+	      try {
+	          String idString = entity.getKey().getName();
+	          UUID id = UUID.fromString(idString);
+	          String user = (String) entity.getProperty("user");
+	          String text = (String) entity.getProperty("text");
+	          long timestamp = (long) entity.getProperty("timestamp");
+	          String recipient = (String) entity.getProperty("recipient");
+	          Message message = new Message(id, user, text, timestamp, recipient);
+	          messages.add(message);
+	      } catch (Exception e) {
+	          System.err.println("Error reading message.");
+	          System.err.println(entity.toString());
+	          e.printStackTrace();
+	      }
 	  }
 	  return messages;
   }
+
 }
