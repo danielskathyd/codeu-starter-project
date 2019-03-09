21b35709bfeda2ebcae2df406909c7c147ccaceb
Kathy Daniels <danielskathyd@gmail.com>
2019-03-08 23:20:53 -0800

    Track user message count

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -3,16 +3,21 @@ package com.google.codeu.data;
 public class User {
 	private String email;
 	private String aboutMe;
-	
+	private int messageCount;
+
 	public User(String email, String aboutMe ) {
 		this.email = email;
 		this.aboutMe = aboutMe;
+		this.messageCount = 0;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
 	public String getAboutMe() {
 		return aboutMe;
+	}
+	public void addMessage() {
+		this.messageCount = this.messageCount + 1;
 	}
 }
