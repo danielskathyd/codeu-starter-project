8e42eb8d5653f4f743b6e3f1f9edfe0519f905d8
Jessica Meng <jessica.meng0402@gmail.com>
2019-04-16 20:33:32 -0700

    attempted to fix

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,35 +1,27 @@
 package com.google.codeu.servlets;
-<<<<<<< HEAD
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+
 import java.io.IOException;
-=======
+
 
 import java.io.IOException;
 import java.util.List;
 
->>>>>>> 45541d9c1cddb6b91408f9447bf075d67f7d0f00
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-/**
- * Handles fetching all messages for the public feed.
- */
-
-
- @WebServlet("/feed")
-public class MessageFeedServlet extends HttpServlet{
-
-=======
-import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Message;
-import com.google.gson.Gson;
 
 /**
  * Handles fetching all messages for the public feed.
  */
-@WebServlet("/feed")
+
+
 public class MessageFeedServlet extends HttpServlet{
 
     private Datastore datastore;
@@ -42,16 +34,12 @@ public class MessageFeedServlet extends HttpServlet{
     /**
      * Responds with a JSON representation of Message data for all users.
      */
->>>>>>> 45541d9c1cddb6b91408f9447bf075d67f7d0f00
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
-<<<<<<< HEAD
-        response.getOutputStream().println("this will be my message feed");
-    }
-}
-=======
+
         response.setContentType("application/json");
 
         List<Message> messages = datastore.getAllMessages();
@@ -61,4 +49,4 @@ public class MessageFeedServlet extends HttpServlet{
         response.getOutputStream().println(json);
     }
 }
->>>>>>> 45541d9c1cddb6b91408f9447bf075d67f7d0f00
+
