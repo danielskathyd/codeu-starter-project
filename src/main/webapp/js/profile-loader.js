01ea3ba30ce39c79f3a5ca2fe5fb5162901e9468
Jessica Meng <jessica.meng0402@gmail.com>
2019-04-17 22:43:03 -0700

    added maps on the profile page, and redirect login to profile page

--- src/main/webapp/js/profile-loader.js
+++ src/main/webapp/js/profile-loader.js
@@ -56,13 +56,62 @@ function fetchProfile(){
 }
 
 
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
 
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
+    var x = document.getElementById("map");
+    function getLocation() {
+        if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+         x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+    }
 
 
 /** Fetches data and populates the UI of the page. */
 function buildUI() {
   setPageTitle();
   fetchProfile();
-
+  initMap();
 
 }
