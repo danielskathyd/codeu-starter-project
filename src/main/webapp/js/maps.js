a5e1e764f08a532ef59df3ea0c5d0c009c55e36b
Jessica Meng <jessica.meng0402@gmail.com>
2019-04-18 13:45:42 -0700

    Should be fixed for maps

--- src/main/webapp/js/maps.js
+++ src/main/webapp/js/maps.js
@@ -1,15 +1,32 @@
-     /** Creates a map that shows landmarks around Google. */
-         function createMap(){
-
-           const map = new google.maps.Map(document.getElementById('map'), {
-             center: {lat: 37.422403, lng: -122.088073},
-             zoom: 15
-           });
-
-           addLandmark(map, 37.423829, -122.092154, 'Google West Campus', 'Google West Campus is home to YouTube and Maps.')
-           addLandmark(map, 37.421903, -122.084674, 'Stan the T-Rex', 'This is Stan, the T-Rex statue.')
-           addLandmark(map, 37.420919, -122.086619, 'Permanente Creek Trail', 'Permanente Creek Trail connects Google to a system of bike trails.');
+     function fetchUsers(map){
+       const urlParams = new URLSearchParams(window.location.search);
+         //const parameterUsername = urlParams.get('user');
+         //const parameterInterest= urlParams.get('interest');
+       const url = '/users';
+       fetch(url).then((response) => {
+         return response.json();
+       }).then((users) => {
+         /**const userContainer = document.getElementById('user-container');
+         if(users.length == 0){
+          userContainer.innerHTML = '<p>No users to display.</p>';
          }
+         else{
+          userContainer.innerHTML = '';
+         } */
+
+         users.forEach((user) => {
+            var latitude = user.latitude ;
+            var longitude = user.longitude;
+            var name     = user.name;
+            var interest = user.interest;
+            addLandMark(map, latitude, longitude, name, interest);
+         });
+       });
+     }
+
+
+     /** Creates a map that shows landmarks around Google. */
+
 
          /** Adds a marker that shows an InfoWindow when clicked. */
 
@@ -27,3 +44,47 @@
              infoWindow.open(map, marker);
            });
          }
+
+     var map, infoWindow;
+           function initMap() {
+             map = new google.maps.Map(document.getElementById('map'), {
+               center: {lat: -34.397, lng: 150.644},
+               zoom: 6
+             });
+             infoWindow = new google.maps.InfoWindow;
+
+             // Try HTML5 geolocation.
+             if (navigator.geolocation) {
+               navigator.geolocation.getCurrentPosition(function(position) {
+                 var pos = {
+                   lat: position.coords.latitude,
+                   lng: position.coords.longitude
+                 };
+
+                 infoWindow.setPosition(pos);
+                 infoWindow.setContent('Location found.');
+                 infoWindow.open(map);
+                 map.setCenter(pos);
+               }, function() {
+                 handleLocationError(true, infoWindow, map.getCenter());
+               });
+             } else {
+               // Browser doesn't support Geolocation
+               handleLocationError(false, infoWindow, map.getCenter());
+             }
+           }
+
+           function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+             infoWindow.setPosition(pos);
+             infoWindow.setContent(browserHasGeolocation ?
+                                   'Error: The Geolocation service failed.' :
+                                   'Error: Your browser doesn\'t support geolocation.');
+             infoWindow.open(map);
+           }
+
+
+      function createMap(){
+
+                 initMap();
+                  fetchUsers(map);
+      }
